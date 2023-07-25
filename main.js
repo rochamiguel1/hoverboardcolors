77735807a1e702a85c5671799380aac53ed2bfa3
rochamiguel1 <miroga2005@gmail.com>
2023-07-25 13:30:10 +0200

    cambios

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 const container = document.querySelector (".container")
 const colors = [ "#49e734", "#42ddf1", "#f05cdf", "#dfdf3a","#40e0d0", "#e43e25" , "#f5f5f5", "#3426f4","#984cdf", "#08ac08", "#d59815","#d59815","#e1145f","#1ce796"]
-const squares = 500;
+const squares = 420;
 
-for(let i = 1 ; i < squares ; i++) {
+for(let i = 0 ; i < squares ; i++) {
   const square = document.createElement("div")
   square.classList.add("square")
 
@@ -23,7 +23,6 @@ function removecolor(e) {
     e.style.background = "#1d1d1d";
     e.style.boxShadow = `0 0 2px #000`
 }
-
 function ramdoncolors() {
     //devuelve un color del array al azar
     return colors[Math.floor(Math.random() * colors.length)]
